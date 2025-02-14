822c73b854a28a4337c076f1b83160dc7cb2e5cc
Nitish kumar <108580546+nit011@users.noreply.github.com>
2025-02-14 11:09:24 +0530

    Update ERC20Mock.sol

--- test/unit/mocks/ERC20Mock.sol
+++ test/unit/mocks/ERC20Mock.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract ERC20Mock is ERC20 {
-    constructor() ERC20("Mock","Mock") {}
+    constructor() ERC20("Mock_Token","Mock_Token") {}
 
     function mint(address to, uint256 amount ) public {
            _mint(to,amount);
